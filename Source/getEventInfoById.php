096b9e3574b6872a4f448882997f718cd0c4c91d
SKM1221 <sorudals@kaist.ac.kr>
2016-07-16 00:28:56 -0700

    change getPosting to getEvent, insert addEvent.php and getEventById.php

---
+++ Source/getEventInfoById.php
@@ -0,0 +1,21 @@
+<?php
+$event_id = $_POST["event_id"];
+$event_id = mysqli_real_escape_string($conn,$event_id);
+$conn = mysqli_connect("localhost:3307", "root", "soso1221", "ucscplaza");
+    if (mysqli_connect_errno($conn)){
+        echo "Failed to connect to MySQL:".mysqli_connect_error();
+}
+
+$query = "SELECT * FROM tbl_event where event_id = ".$event_id;
+
+$result = $conn->query($query) or die($mysqli->error.__LINE__);
+
+$row = $result->fetch_assoc();
+$small=array('startDate'=>$row["event_start_date"],'endDate'=>$row["event_end_date"],'category'=>$row["event_category"],'major'=>$row["event_major"],'maxAttendance'=>$row["event_max_att"],'currentAttendance'=>$row["event_current_att"],'image'=>$row["event_file"],'description'=>$row["event_descrition"],'details'=>$row["event_details"]);
+
+echo json_encode($small);
+
+mysqli_close($conn);
+
+?>
+
